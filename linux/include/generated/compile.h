2ea24f3f2ab0faf033b714af0019eb3b76274c57
sNullp <snullp@gmail.com>
2014-02-23 19:27:59 -0500

    change codes to symlink

--- linux/include/generated/compile.h
+++ linux/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Sat Feb 22 16:59:40 EST 2014"
+#define UTS_VERSION "#3 PREEMPT Sun Feb 23 00:34:23 EST 2014"
 #define LINUX_COMPILE_BY "null"
 #define LINUX_COMPILE_HOST "CLINTON"
 #define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
