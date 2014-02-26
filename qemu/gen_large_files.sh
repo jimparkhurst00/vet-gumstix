6c70bb64f536009a9944bfef48c99e9fe3db4446
sNullp <snullp@gmail.com>
2014-02-25 23:35:52 -0500

    removal of big files cont. - finished the auto download & generate script

--- qemu/gen_large_files.sh
+++ qemu/gen_large_files.sh
@@ -5,5 +5,18 @@ wget http://s3-us-west-2.amazonaws.com/gumstix-yocto/Releases/2013-08-09/overo/d
 wget http://s3-us-west-2.amazonaws.com/gumstix-yocto/Releases/2013-08-09/overo/dev/u-boot.img
 wget --output-document=uImage.oem http://s3-us-west-2.amazonaws.com/gumstix-yocto/Releases/2013-08-09/overo/dev/uImage
 
+mkdir tmpdir
+cd tmpdir
+MOD_INSTALL_DIR=`pwd`
+cd ..
+
 cd ../linux
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules -j4
+sudo INSTALL_MOD_PATH=${MOD_INSTALL_DIR} make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules_install
+cd ../qemu
+
+cd tmpdir
+tar cfzv ../modules.tgz *
+cd ..
+
+sudo rm -r tmpdir
