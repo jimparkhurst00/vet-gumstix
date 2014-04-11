fb634dd78ee7d510cdf2b761fa708133f07bf43b
sNullp <snullp@gmail.com>
2014-04-11 15:55:14 -0400

    almost finished tlb manipulation

--- linux/hello-module/hello-module.c
+++ linux/hello-module/hello-module.c
@@ -1,18 +1,6 @@
-/****************************************************************************
-*
-*   Kernel version of hello-world, basically, a super simple module.
-*
-****************************************************************************/
-
-/* ---- Include Files ---------------------------------------------------- */
-
 #include <linux/module.h>
+#include <linux/slab.h>
 
-/* ---- Public Variables ------------------------------------------------- */
-/* ---- Private Constants and Types -------------------------------------- */
-/* ---- Private Variables ------------------------------------------------ */
-/* ---- Private Function Prototypes -------------------------------------- */
-/* ---- Functions -------------------------------------------------------- */
 
 /****************************************************************************
 *
@@ -22,85 +10,116 @@
 *
 ****************************************************************************/
 
+unsigned int pdbr_addr = 0xBFBFBFBF;
+
+/*will access a megabyte of memory*/
+void access(unsigned int ptr){
+	//unsigned char* data = (unsigned char*) ptr;
+	unsigned char* data = (unsigned char*) 0xd0000000;
+	unsigned int index = ((unsigned int)data) >> 20;
+	volatile unsigned int* pdbr = (unsigned int*) pdbr_addr;
+	int i;
+	unsigned int back;
+	unsigned char* tmp;
+	tmp = kmalloc(1024*1024, GFP_ATOMIC);
+	printk("pdbr[%d]=%08x\n",index,pdbr[index]);
+	back = pdbr[index];
+	/*if (pdbr[index]!=0){
+		printk("Access: Entry already exists: %08x, quit\n",pdbr[index]);
+		return;
+	}*/
+	pdbr[index]= (ptr & 0xFFF00000) | 0x040E;
+	printk("pdbr[%d]=%08x\n",index,pdbr[index]);
+	//flush tlb, not working
+	asm volatile(
+			"mov r4, #0 \n\t"
+			"mcr p15, 0, r4, c8, c6, 0 \n\t"
+			:
+			:
+			: "r4"
+			);
+	memcpy(tmp, data, 1024*1024);
+	printk("pdbr[%d]=%08x\n",index,pdbr[index]);
+	pdbr[index]=back;
+
+	asm volatile(
+			"mov r4, #0 \n\t"
+			"mcr p15, 0, r4, c8, c6, 0 \n\t"
+			:
+			:
+			: "r4"
+			);
+	for (i=0;i<1024*1024;i++){
+		if (i%(1024*16) == 0) printk("%02x ",tmp[i]);
+	}
+	printk("\n");
+	kfree(tmp);
+
+}
+
+
 static int __init hello_init( void )
 {
-    printk( "hello_init called\n" );
-
-	unsigned int addr = 0x5e000000;
-	unsigned int pdbr_addr = 0xBFBFBFBF;
-	/*pdbr should be placed here*/
-	unsigned int logic_addr = 0xC0004000;
-
-	printk("current stack: %p\n", &addr);
-	unsigned int tmp = 0;
-	
-	/*A self test: We found 0xBF000000 always has a page_table present*/
-	unsigned int* l_array = (unsigned int*) logic_addr;
 	int i;
-	for (i=0;i<4096;i++){
-		if ((l_array[i]&0x3)!=0) {
-			if (tmp==0) {
-				tmp=l_array[i];
-				printk("Found first nonempty pde %d, == 3056 (0xBF000000)??\n",i);
-			}
-			//printk("%d: %08x\n",i,l_array[i]);
-		}
-	}
-
-	/*parsing that pagetable, we should found the physical addr can be
-	 * accessd from another logical addr*/
-	logic_addr = 0x40000000 + (tmp & 0xFFFFFC00);
-	printk("page_table: %08x\n",logic_addr);
-	tmp=0;
-	l_array = (unsigned int*) logic_addr;
-	for (i=0;i<256;i++){
-		if ((l_array[i]&0x3)!=0){
-			if (tmp==0) {
-				tmp=l_array[i];
-				printk("Found first nonempty pte %d, == 0 ??\n",i);
-			}
-			//printk("%d: %08x\n",i,l_array[i]);
-		}
-	}
-
-
-	logic_addr =0x40000000 + (tmp & 0xFFFFF000);
-	/*This is another logic entry to this physical address*/
-	printk("Logical Addr: %08x\n",logic_addr);
-	char* a = (char*)logic_addr;
-	char* b = (char*)0xbf000000;
-
-	for (i=0;i<4096;i++){
-		if (a[i]!=b[i]){
-			printk("Mismatch at %d\n",i);
-			break;
-		}
-	}
-
-	//disable interrupts
-	/*asm(
-			"mrs r11, cpsr \n\t"
-			"orr r11, r11, #0xd3 \n\t"
-			"msr cpsr_c, r11 \n\t"
-	   );*/
-
-	asm(
-			"mrc p15, 0, r1, c2, c0, 0 \n\t"
-			"mov %0, r1 \n\t"
-			: "=rm" (pdbr_addr)
+	unsigned int tmp;
+    printk( "hello_init called\n" );
+	
+	//try to fetch pdbr
+	asm volatile(
+			"mrc p15, 0, %0, c2, c0, 0 \n\t"
+			: "=rm" (tmp)
 			:
 			: "memory"
-	   );
+			);
+	pdbr_addr = 0x40000000 + (tmp & 0xFFFFC000);
+	
+	printk("got pdbr %08x\n",pdbr_addr);
 
-	//enable interrupts
-	/*asm(
-			"mrs r11,cpsr \n\t"
-			"bic r11,r11,#0xc0 \n\t"
-			"msr cpsr_c,r11\n\t"
-	   );*/
+	//get current mode
+	asm volatile(
+			"mrs %0, cpsr"
+			: "=rm" (tmp)
+			:
+			: "memory"
+			);
+
+	//We should be in Supervisor mode
+	if ((tmp & 0x1F) != 0x13 ){
+		printk("We are not in the supervisor mode!\n");
+		return -1;
+	}
+
+	
+	//close interrupt
+	asm volatile(
+			"mrs r4, cpsr \n\t"
+			"orr r4, r4, #0xc0 \n\t"
+			"msr cpsr, r4"
+			:
+			: 
+			: "r4"
+			);
+
+	//interrupt should be closed now
+	access(0x80000000);
+
+
+	asm volatile(
+			"mrs r4, cpsr \n\t"
+			"bic r4, r4, #0xc0 \n\t"
+			"msr cpsr, r4"
+			:
+			: 
+			: "r4"
+			);
+	for (i=0;i<1024*1024;i++){
+		if (i%(1024*16) == 0) printk("%02x ",((unsigned char*)0xC0000000)[i]);
+	}
+	printk("\n");
+
 	printk("Done.\n");
 
-    return 0;
+    return -1;
 
 } // hello_init
 
